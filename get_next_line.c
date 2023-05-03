639f668f012f4d4da84ff878ae5aef0608c9637b
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2023-05-03 14:07:11 +0100

    working only with BUFFER_SIZE=1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 21:09:13 by aantonio          #+#    #+#             */
-/*   Updated: 2023/04/29 12:23:45 by aantonio         ###   ########.fr       */
+/*   Updated: 2023/05/03 14:05:10 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*get_next_line(int fd)
 
 	line_length = 1;
 	current_line = malloc(sizeof(char) * (line_length + 1));
-	if (read(fd, current_line, BUFFER_SIZE) != BUFFER_SIZE)
+	if (read(fd, current_line, 1) != 1)
 	{
 		free(current_line);
 		return (NULL);
@@ -34,14 +34,8 @@ char	*get_next_line(int fd)
 			j++;
 		}
 		ft_memcpy(line_copy, current_line, line_length);
-		char readchar;
-		int result = read(fd, &readchar, BUFFER_SIZE) ;
-		if (result != BUFFER_SIZE)
-		{
-
+		if (read(fd, &line_copy[line_length], 1) != 1)
 			break ;
-		}
-		line_copy[line_length] = readchar;
 		free(current_line);
 		current_line = malloc(sizeof(char) * (line_length + 2));
 		ft_memcpy(current_line, line_copy, line_length + 1);
